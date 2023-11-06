1cad9cce619f71f70c1fd04f5742733b9f76914e
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-11-06 15:22:48 +0100

    I expect it works on fl0

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-import { MongoClient } from 'mongodb' // Documentación CRUD: https://mongodb.github.io/node-mongodb-native/6.2/
-import express, { request, response } from "express"
+import { MongoClient, ObjectId } from 'mongodb' // Documentación CRUD: https://mongodb.github.io/node-mongodb-native/6.2/
+import express from "express"
 
 const app = express()
 app.use(express.json())  // IMPORTANTE: SOPORTE PARA JSON
 
 
+const PORT = process.env.PORT ?? 3000
 const DB_URL = process.env.MONGODB_URI ?? 'mongodb://localhost:27017'
 const DB_NAME = process.env.DB_NAME ?? 'api'
 const COLLECTION = 'users'
-const PORT = process.env.PORT ?? 3000
-
-// let mongoClient = null
-// let database = null
-
-// Si tenemos conexión anterior, la reutilizamos
-// async function connectToDatabase() {
-//     try {
-//         if (mongoClient && database) {
-//             return { mongoClient, database };
-//         }
-//         mongoClient = await (new MongoClient(DB_URL)).connect();
-//         database = await mongoClient.db(DB_NAME);
-//         return { mongoClient, database };
-//     } catch (e) {
-//         console.error(e);
-//     }
-// }
 
 const client = new MongoClient(DB_URL)
-const database = null
-
-async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-    //   await client.connect(); 
-      // Send a ping to confirm a successful connection
-      const database = await client.db(DB_NAME);
-      console.log("Conectamos a MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-      console.log('Salimos');
-    }
-  }
-  run().catch(console.dir);
-
 
 app.get("/", (request, response) => {
     response.redirect("/api/users")
@@ -53,7 +19,7 @@ app.get("/", (request, response) => {
 
 // GET
 app.get('/api/users', async (request, response) => {
-    const { database } = await connectToDatabase();
+    const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
     const results = await collection.find({}).toArray()
@@ -66,7 +32,7 @@ app.post('/api/users', async (request, response) => {
     if (!request.is('json'))
         return response.json({ message: 'Debes proporcionar datos JSON' })
 
-    const { database } = await connectToDatabase();
+    const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
     const { nombre, edad } = request.body
@@ -77,11 +43,11 @@ app.post('/api/users', async (request, response) => {
 
 // GET 
 app.get('/api/users/:id', async (request, response) => {
-    const { database } = await connectToDatabase();
+    const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
     const { id } = request.params
-    const results = await collection.find({ _id: id }).toArray()
+    const results = await collection.find({ _id: new ObjectId(id) }).toArray()
 
     response.status(200).json(results)
 })
@@ -91,23 +57,23 @@ app.put('/api/users/:id', async (request, response) => {
     if (!request.is('json'))
         return response.json({ message: 'Debes proporcionar datos JSON' })
 
-    const { database } = await connectToDatabase();
+    const database = client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
     const { id } = request.params
     const { nombre, edad } = request.body
-    const results = await collection.updateOne({ _id: id }, { $set: { nombre, edad } });
+    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { nombre, edad } });
 
     response.status(200).json(results)
 })
 
 // DELETE
 app.delete('/api/users/:id', async (request, response) => {
-    const { database } = await connectToDatabase();
+    const database =  client.db(DB_NAME);
     const collection = database.collection(COLLECTION);
 
     const { id } = request.params
-    const results = await collection.deleteOne({ _id: id }).toArray()
+    const results = await collection.deleteOne({ _id: new ObjectId(id) })
     response.status(200).json(results)
 })
 
